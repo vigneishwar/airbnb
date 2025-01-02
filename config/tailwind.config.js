769bdc4f57f84638f060e50b9456749215e279ad
vikki1310 <156143195+vikki1310@users.noreply.github.com>
2025-01-02 22:45:50 +1100

    added tailwindcss and cleaned gem file

---
+++ config/tailwind.config.js
@@ -0,0 +1,32 @@
+/* @type {import('tailwindcss/tailwind-config').TailwindConfig} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  content: [
+    './public/*.html',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/views/**/*.{erb,haml,html,slim}'
+  ],
+  theme: {
+    colors: {
+      ...colors,
+      primary: {
+        DEFAULT: '#FF5A5F',
+        dark: '#ff385c',
+      }
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/container-queries'),
+  ]
+}
